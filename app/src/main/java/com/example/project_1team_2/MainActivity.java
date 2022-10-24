d0b2317f1618b0f81122e99120532377c19f3001
Bean <bddang@svsu.edu>
2022-10-24 18:49:25 -0400

    Binh trying commit

--- app/src/main/java/com/example/project_1team_2/MainActivity.java
+++ app/src/main/java/com/example/project_1team_2/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Binh is here
+        //Binh is here 6.48
     }
 }
