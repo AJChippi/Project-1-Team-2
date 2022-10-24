2d1144b92f5bfde62eb0afdf18c66bdcc10c7708
Anthony Chippi <chippi.anthony@gmail.com>
2022-10-24 18:56:04 -0400

    added button

--- app/src/main/java/com/example/project_1team_2/MainActivity.java
+++ app/src/main/java/com/example/project_1team_2/MainActivity.java
@@ -3,14 +3,26 @@ package com.example.project_1team_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button click;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //chippi is here
+        click = findViewById(R.id.click);
+
+        click.setOnClickListener(view -> {
+            Log.d("TAG", "onCreate: ");
+        });
+
+
     }
 }
