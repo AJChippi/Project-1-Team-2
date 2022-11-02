bc8ea635564b6eabb730a032a02c255bbaad4632
Rakibul Hassan <rakibulh702@gmail.com>
2022-11-02 19:26:56 -0400

    update locations

--- app/src/main/java/com/example/project_1team_2/differentLocations.java
+++ app/src/main/java/com/example/project_1team_2/differentLocations.java
@@ -2,7 +2,6 @@ package com.example.project_1team_2;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.SearchView;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -25,7 +25,6 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class differentLocations extends AppCompatActivity {
-
     RecyclerView locationList;
     ArrayList<Locations> locationsArrayList;
     String searchName = "";
@@ -38,12 +37,10 @@ public class differentLocations extends AppCompatActivity {
     String tempLowest;
     RequestQueue queue;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_different_locations);
-
         locationsArrayList = new ArrayList<>();
         locationList = findViewById(R.id.locationList);
         queue = Volley.newRequestQueue(this);
@@ -124,6 +121,31 @@ public class differentLocations extends AppCompatActivity {
 //        locationsArrayList.add(new Locations("Tampa", "15:15","Mostly Clear",78.5,69.8,95));
 //        locationsArrayList.add(new Locations("Miami", "15:15","Mostly Clear",78.5,69.8,95));
 //        locationsArrayList.add(new Locations("Dhaka", "15:15","Mostly Clear",78.5,69.8,95));
+
+
+        LocationAdapter adapter = new LocationAdapter(locationsArrayList,this);
+        locationList.setAdapter(adapter);
+        locationList.setLayoutManager(new LinearLayoutManager(this));
+        adapter.notifyDataSetChanged();
+
+        /**
+         * Swipe to remove item from location list
+         */
+        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                locationsArrayList.remove(viewHolder.getLayoutPosition());
+                adapter.notifyDataSetChanged();
+
+            }
+        });
+        helper.attachToRecyclerView(locationList);
+
 
 
     }
