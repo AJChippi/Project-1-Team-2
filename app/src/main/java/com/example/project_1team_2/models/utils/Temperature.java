19e5d62dc937dbd6eed44fbf3007ff2202abff24
Izeyaan <91957066+yanyun21@users.noreply.github.com>
2022-11-04 19:57:16 -0400

    hoping (#37)
    Co-authored-by: Izaah Idham <izaahidham@Idhams-MacBook-Air.local>

---
+++ app/src/main/java/com/example/project_1team_2/models/utils/Temperature.java
@@ -0,0 +1,7 @@
+package com.example.project_1team_2.models.utils;
+
+public class Temperature {
+    static public float fahrenheitToCelsius(float f) {
+        return (f - 32f) * 5f / 9f;
+    }
+}
